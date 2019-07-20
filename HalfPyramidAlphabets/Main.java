f59ec8c0fd426cd396b1d5376656b5151b39a628
ashutoshpith <amxdp18@gmail.com>
2019-07-21 00:24:11 +0530

    half pyramid with number,alphabet,nd reverse

---
+++ HalfPyramidAlphabets/Main.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void main(String[] args) {
+        char first='A',last ='E';
+        
+        for(int i=1; i<= (last-'A'+1); ++i){
+            for(int j=1; j<=i; ++j){
+                System.out.print(first+ " ");
+            }
+            ++first;
+              System.out.println();
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+import HalfPyramid from ./HalfPyramid.*;
+class Main{
+    public static void main(String[] args) {
+        HalfPyramid hp =new HalfPyramid();
+        int a=5;
+        hp.hP(a);
+    }
+}
